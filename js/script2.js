9cbb3a75c3f98e2f9c5147c98e963446682e62f2
ignacioborraz <ignacioborraz@hotmail.com>
2022-07-19 15:12:02 -0300

    correcciones 2

--- js/script2.js
+++ js/script2.js
@@ -10,8 +10,6 @@ let nameProductC = "Photos";
 let precioProductC = 800;
 let stockProductC = 10;
 
-<<<<<<< HEAD
-
 let cantidadDeCompras = parseInt(prompt("¿Que cantidad de productos desea llevar el dia de hoy? \n- 1 \n- 2 \n- 3"))
 let precioFinal = 0;
 
@@ -28,123 +26,55 @@ function calculoStock(cantidad, stock, precio){
         alert("No tenemos en stock ese producto en este momento. Nuestro stock actual es de: " + stock + " unidades")
     }
 }
-=======
-//tratemos de usar SIEMPRE un único idioma para todo
-let cantidadDeCompras = prompt("¿Que cantidad de productos desea llevar el dia de hoy? \n- 1 \n- 2 \n- 3") //usar parseInt
-let priceFinal = 0;
->>>>>>> 3abc11d0c5ff53f5c5dd1cc03fa3a56452e7627d
 
 //que sucede si el usuario por error teclea una letra?
 //incluir la iteración FOR adentro de un condicional (si es un numero que haga tal cosa y sino: que haga tal otra ==> volver a preguntar)
 for(i = 0; i < cantidadDeCompras ; i++){
-<<<<<<< HEAD
 
     let CompraInicio = prompt("¿Cual de nuestros productos disponibles quiere comprar? Porfavor elija entre estas opciones: \n- Renders \n- Animations \n- Photos")
-    let cantidadBuy = parseInt(prompt("Excelente, ¿Cuantos quiere comprar? "))
-
-    if(CompraInicio == "Renders"){
-        calculoStock(cantidadBuy, stockProductA, precioProductA)
-       
+    //utilizar camelCase en lugar de PascalCase
+    //let cantidadBuy = parseInt(prompt("Excelente, ¿Cuantos quiere comprar? "))
+    //esta variable está mal definida acá ya que si el usuario teclea cualquier cosa: me pregunta igualmente CUANTOS QUIERE
+    //y eso está mal
+    if(CompraInicio == "Renders"){ //usar los métodos correspondientes para comparar correctamente
+        let cantidadBuy = parseInt(prompt("Excelente, ¿Cuantos quiere comprar? ")) //acá está usada correctamente
+        calculoStock(cantidadBuy, stockProductA, precioProductA) //y mejor seria hacer el pedido adentro de la función
     }
-    else if(CompraInicio == "Animations"){
+    else if(CompraInicio == "Animations"){ let cantidadBuy = parseInt(prompt("Excelente, ¿Cuantos quiere comprar? ")) //idem comentario anterior
         calculoStock(cantidadBuy , stockProductB, precioProductB)
-       
     }
     else if(CompraInicio == "Photos"){
+        let cantidadBuy = parseInt(prompt("Excelente, ¿Cuantos quiere comprar? ")) //idem comentario anterior
         calculoStock(cantidadBuy, stockProductC, precioProductC)
-=======
-    //los nombres de las variables con camelCase (PascalCase se utilizará en REACT y para otro tipo de "variables")
-    let StartBuy = prompt("¿Cual de nuestros productos disponibles quiere comprar? Porfavor elija entre estas opciones: \n- Renders \n- Animations \n- Photos")
-    let cantidadBuy = parseInt(prompt("Excelente, ¿Cuantos quiere comprar? "))
-    //alert(" Perfecto, en caso de superar un monto de 5000, estara participando de un sorteo. Le deseamos suerte.") //esta alerta es innecesaria (genera una mala UX)
-
-    if(StartBuy == "Renders"){ //utilizar los métodos aprendidos para mejorar esta condicion
-        if(stockProductA >= cantidadBuy){
-            priceFinal += cantidadBuy * precioProductA
-            alert("Su precio es de : $" + (cantidadBuy * precioProductA))
-        }
-        else{
-            alert("Ahora mismo, no disponemos de ese stock. A continuacion nuestro stock actual es de: " + stockProductA + "Entrantes")
-        }
-       
-    }
-    else if(StartBuy == "Animations"){ //es igual a renders, emplear una funcion
-        if(stockProductB >= cantidadBuy){
-            priceFinal += cantidadBuy * precioProductB
-            alert("Su precio es de : $" + (cantidadBuy * precioProductB))
-        }
-        else{
-            alert("Ahora mismo, no disponemos de ese stock. A continuacion nuestro stock actual es de: " + stockProductA + "Entrantes")
-        }
-       
-    }
-    else if(StartBuy == "Photos"){ //es igual a los dos anterior, usar una función
-        if(stockProductC >= cantidadBuy){
-            priceFinal += cantidadBuy * precioProductC
-            alert("Su precio es de : $" + (cantidadBuy * precioProductC))
-        }
-        else{
-            alert("Ahora mismo, no disponemos de ese stock. A continuacion nuestro stock actual es de: " + stockProductA + "Entrantes")
-        }
->>>>>>> 3abc11d0c5ff53f5c5dd1cc03fa3a56452e7627d
-       
     }
     else{
         alert("Lo sentimos, no contamos con ese producto")
-        
     }
-      
 }
-<<<<<<< HEAD
 
-console.log(precioFinal)
+//console.log(precioFinal) //comentar los console.log una vez que verificamos que se cargó bien la variable
 switch(true){ 
-    case precioFinal < 1000:
-        precioFinal = precioFinal * 0.90
+    case precioFinal < 1000: //esta condicion creo que no sería necesaria que tenga un descuento
+        precioFinal = precioFinal * 0.90 //esto se repite: usar una función que tenga como parámetro el % de descuento
         alert("Perfecto, ademas te regalamos un 10% de descuento por tu compra!")
         break;
-    case precioFinal <= 1000 || precioFinal < 3000:
+    case precioFinal <= 1000 || precioFinal < 3000: //los signos <= tenés que cambiarlo por >= (para indicar correctamente el intervalo)
         precioFinal = precioFinal * 0.85
         alert("Perfecto, ademas te regalamos un 15% de descuento por tu compra!")
         break;
-    case precioFinal <= 3000 || precioFinal < 7000: 
+    case precioFinal <= 3000 || precioFinal < 7000: //los signos <= tenés que cambiarlo por >= (para indicar correctamente el intervalo)
         precioFinal = precioFinal * 0.70
         alert("Perfecto, ademas te regalamos un 30% de descuento por tu compra!")
         break;
-    default: 
+    default: //esta condicion se va a dar para valores mayores a 7000 y no es un error
+        //deberías investigar la forma de que si en lugar de un numero ingresa una letra o numero negativo
+        //indique este error
         console.log(precioFinal)
-=======
-console.log(priceFinal)
-switch(true){ //ya que estamos trabajando con numeros, ordenemos las condiciones de menor a mayor
-    case priceFinal < 1000:
-        priceFinal = priceFinal * 0.90
-        alert("Perfecto, ademas te regalamos un 10% de descuento por tu compra!")
-        break;
-    case priceFinal <= 1000 || priceFinal < 3000: //corregí esta condicion
-        priceFinal = priceFinal * 0.85
-        alert("Perfecto, ademas te regalamos un 15% de descuento por tu compra!")
-        break;
-    case priceFinal <= 3000 || priceFinal < 7000: //corregí esta condicion
-        priceFinal = priceFinal * 0.70
-        alert("Perfecto, ademas te regalamos un 30% de descuento por tu compra!")
-        break;
-    case priceFinal >= 7000: //corregí esta condicion
-        priceFinal = priceFinal * 0.50
-        alert("Perfecto, ademas te regalamos un 50% de descuento por tu compra!")
-        break;
-    default: //cuando sucedería esto??
-        console.log(priceFinal)
->>>>>>> 3abc11d0c5ff53f5c5dd1cc03fa3a56452e7627d
         alert("Estamos confundidos, porfavor ingresa un precio en numeros!")
         break;
 }
-    
-
 
 function aplicoIva(price){
-<<<<<<< HEAD
-    console.log(price)
-=======
     console.log(price) //consologueá siempre para chequear como entra el parámetro en la función o para verificar una correcta operación
     return price * 1.21
 }
