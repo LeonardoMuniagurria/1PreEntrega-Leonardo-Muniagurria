5f168b91def0ae88bbef2d1283fc6fe1ef3383fd
LeonardoMuniagurria <leoyro12033@gmail.com>
2022-07-25 15:30:08 -0300

    Arrays

--- js/script2.js
+++ js/script2.js
@@ -1,92 +1,75 @@
-let nameProductA = "Renders";
-let precioProductA = 500;
-let stockProductA = 15;
+function productos(nombre, stock, precio){
+    this.nombre = nombre
+    this.stock = stock
+    this.precio = precio 
+    this.iva = function (precio){
+        console.log(precio)
+        return precio * 1.21
+    }
+}
 
-let nameProductB = "Animations";
-let precioProductB = 1500;
-let stockProductB = 10
+const productoA = new productos("Renders", 15, 500)
+const productoB = new productos("Animations", 10, 1500)
+const productoC = new productos("Photos", 10, 800)
+const productoE = new productos("Peluches", 15, 1500)
 
-let nameProductC = "Photos";
-let precioProductC = 800;
-let stockProductC = 10;
 
-<<<<<<< HEAD
+let productosVentas = [productoA, productoB, productoC, productoE]
 
-let cantidadDeCompras = parseInt(prompt("¿Que cantidad de productos desea llevar el dia de hoy? \n- 1 \n- 2 \n- 3"))
+productosVentas.push(new productos("Videos", 20, 600))
+
+let nuevosProductos = []
+
+function listaProductos(){
+    for(const productos of productosVentas){
+        nuevosProductos.push(productos.nombre)
+    }
+}
+listaProductos()
+
+
+productoA.iva()
+productoB.iva()
+productoC.iva()
+
+
+let cantidadDeCompras = parseInt(prompt("¿Que cantidad de productos desea llevar el dia de hoy? \n" + nuevosProductos.join("\n")))
 let precioFinal = 0;
 
 function calculoPrecio(cantidad, precio){
     precioFinal += cantidad * precio
 }
 
-function calculoStock(cantidad, stock, precio){
-    if(stock >= cantidad){
-        calculoPrecio(cantidad, precio)
-        alert("El precio total es de: $" + (cantidad * precio))
+
+function calculoStock(cantidad, productos){
+    if(productos.stock >= cantidad){
+        calculoPrecio(cantidad, productos.precio)
+        alert("El precio total de tu producto es de: " + cantidad * productos.precio)
     }
     else{
-        alert("No tenemos en stock ese producto en este momento. Nuestro stock actual es de: " + stock + " unidades")
+        alert("No tenemos en stock ese producto en este momento. Nuestro stock actual es de: " + productos.stock + " unidades")
     }
 }
-=======
-//tratemos de usar SIEMPRE un único idioma para todo
-let cantidadDeCompras = prompt("¿Que cantidad de productos desea llevar el dia de hoy? \n- 1 \n- 2 \n- 3") //usar parseInt
-let priceFinal = 0;
->>>>>>> 3abc11d0c5ff53f5c5dd1cc03fa3a56452e7627d
+
+
 
 //que sucede si el usuario por error teclea una letra?
 //incluir la iteración FOR adentro de un condicional (si es un numero que haga tal cosa y sino: que haga tal otra ==> volver a preguntar)
 for(i = 0; i < cantidadDeCompras ; i++){
-<<<<<<< HEAD
 
     let CompraInicio = prompt("¿Cual de nuestros productos disponibles quiere comprar? Porfavor elija entre estas opciones: \n- Renders \n- Animations \n- Photos")
     let cantidadBuy = parseInt(prompt("Excelente, ¿Cuantos quiere comprar? "))
 
-    if(CompraInicio == "Renders"){
-        calculoStock(cantidadBuy, stockProductA, precioProductA)
+    if(CompraInicio == productoA.nombre){
+        calculoStock(cantidadBuy, productoA)
        
     }
-    else if(CompraInicio == "Animations"){
-        calculoStock(cantidadBuy , stockProductB, precioProductB)
+    else if(CompraInicio == productoB.nombre){
+        calculoStock(cantidadBuy , productoB)
        
     }
-    else if(CompraInicio == "Photos"){
-        calculoStock(cantidadBuy, stockProductC, precioProductC)
-=======
-    //los nombres de las variables con camelCase (PascalCase se utilizará en REACT y para otro tipo de "variables")
-    let StartBuy = prompt("¿Cual de nuestros productos disponibles quiere comprar? Porfavor elija entre estas opciones: \n- Renders \n- Animations \n- Photos")
-    let cantidadBuy = parseInt(prompt("Excelente, ¿Cuantos quiere comprar? "))
-    //alert(" Perfecto, en caso de superar un monto de 5000, estara participando de un sorteo. Le deseamos suerte.") //esta alerta es innecesaria (genera una mala UX)
-
-    if(StartBuy == "Renders"){ //utilizar los métodos aprendidos para mejorar esta condicion
-        if(stockProductA >= cantidadBuy){
-            priceFinal += cantidadBuy * precioProductA
-            alert("Su precio es de : $" + (cantidadBuy * precioProductA))
-        }
-        else{
-            alert("Ahora mismo, no disponemos de ese stock. A continuacion nuestro stock actual es de: " + stockProductA + "Entrantes")
-        }
-       
-    }
-    else if(StartBuy == "Animations"){ //es igual a renders, emplear una funcion
-        if(stockProductB >= cantidadBuy){
-            priceFinal += cantidadBuy * precioProductB
-            alert("Su precio es de : $" + (cantidadBuy * precioProductB))
-        }
-        else{
-            alert("Ahora mismo, no disponemos de ese stock. A continuacion nuestro stock actual es de: " + stockProductA + "Entrantes")
-        }
-       
-    }
-    else if(StartBuy == "Photos"){ //es igual a los dos anterior, usar una función
-        if(stockProductC >= cantidadBuy){
-            priceFinal += cantidadBuy * precioProductC
-            alert("Su precio es de : $" + (cantidadBuy * precioProductC))
-        }
-        else{
-            alert("Ahora mismo, no disponemos de ese stock. A continuacion nuestro stock actual es de: " + stockProductA + "Entrantes")
-        }
->>>>>>> 3abc11d0c5ff53f5c5dd1cc03fa3a56452e7627d
+    else if(CompraInicio == productoC.nombre){
+        calculoStock(cantidadBuy, productoC)
        
     }
     else{
@@ -95,59 +78,3 @@ for(i = 0; i < cantidadDeCompras ; i++){
     }
       
 }
-<<<<<<< HEAD
-
-console.log(precioFinal)
-switch(true){ 
-    case precioFinal < 1000:
-        precioFinal = precioFinal * 0.90
-        alert("Perfecto, ademas te regalamos un 10% de descuento por tu compra!")
-        break;
-    case precioFinal <= 1000 || precioFinal < 3000:
-        precioFinal = precioFinal * 0.85
-        alert("Perfecto, ademas te regalamos un 15% de descuento por tu compra!")
-        break;
-    case precioFinal <= 3000 || precioFinal < 7000: 
-        precioFinal = precioFinal * 0.70
-        alert("Perfecto, ademas te regalamos un 30% de descuento por tu compra!")
-        break;
-    default: 
-        console.log(precioFinal)
-=======
-console.log(priceFinal)
-switch(true){ //ya que estamos trabajando con numeros, ordenemos las condiciones de menor a mayor
-    case priceFinal < 1000:
-        priceFinal = priceFinal * 0.90
-        alert("Perfecto, ademas te regalamos un 10% de descuento por tu compra!")
-        break;
-    case priceFinal <= 1000 || priceFinal < 3000: //corregí esta condicion
-        priceFinal = priceFinal * 0.85
-        alert("Perfecto, ademas te regalamos un 15% de descuento por tu compra!")
-        break;
-    case priceFinal <= 3000 || priceFinal < 7000: //corregí esta condicion
-        priceFinal = priceFinal * 0.70
-        alert("Perfecto, ademas te regalamos un 30% de descuento por tu compra!")
-        break;
-    case priceFinal >= 7000: //corregí esta condicion
-        priceFinal = priceFinal * 0.50
-        alert("Perfecto, ademas te regalamos un 50% de descuento por tu compra!")
-        break;
-    default: //cuando sucedería esto??
-        console.log(priceFinal)
->>>>>>> 3abc11d0c5ff53f5c5dd1cc03fa3a56452e7627d
-        alert("Estamos confundidos, porfavor ingresa un precio en numeros!")
-        break;
-}
-    
-
-
-function aplicoIva(price){
-<<<<<<< HEAD
-    console.log(price)
-=======
-    console.log(price) //consologueá siempre para chequear como entra el parámetro en la función o para verificar una correcta operación
-    return price * 1.21
-}
-
-let impuestoAplicado = aplicoIva(precioFinal)
-alert ("El precio final con impuestos es de: " + impuestoAplicado)
