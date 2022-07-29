b2fa63489a4253a6a49922bcd3407f63810df73c
LeonardoMuniagurria <leoyro12033@gmail.com>
2022-07-29 10:03:13 -0300

    TareaRealizada

--- js/script2.js
+++ js/script2.js
@@ -1,25 +1,24 @@
-function productos(nombre, stock, precio){ //las funciones constructoras se definen con PascalCase (es decir, arrancan con mayúsculas)
+function Productos (nombre, stock, precio){ //las funciones constructoras se definen con PascalCase (es decir, arrancan con mayúsculas)
     this.nombre = nombre //seria mejor usar CLASE que funcion constructora
     this.stock = stock
     this.precio = precio 
-    this.iva = function (precio){
-        console.log(precio)
-        return precio * 1.21
-    }
-    //this.precioMasIva = precio*1.21 //seria mejor asignar una nueva propiedad mas que un nuevo modelo, pero eso va a depender de lo que quieras hacer
+    this.precioMasIva = precio*1.21
 }
-
-const productoA = new productos("Renders", 15, 500)
-const productoB = new productos("Animations", 10, 1500)
-const productoC = new productos("Photos", 10, 800)
-const productoE = new productos("Peluches", 15, 1500)
+    //this.precioMasIva = precio*1.21 //seria mejor asignar una nueva propiedad mas que un nuevo modelo, pero eso va a depender de lo que quieras hacer
 
 
-let productosVentas = [productoA, productoB, productoC, productoE]
+const productoA = new Productos("Renders", 15, 500)
+const productoB = new Productos("Animations", 10, 1500)
+const productoC = new Productos("Photos", 10, 800)
+const productoE = new Productos("Peluches", 15, 1500)
+const productoD = new Productos("Videos", 20, 600)
 
-productosVentas.push(new productos("Videos", 20, 600))
 
+let productosVentas = [productoA, productoB, productoC, productoE, productoD]
 let nuevosProductos = []
+console.log(nuevosProductos)
+productosVentas.push(nuevosProductos)
+
 
 function listaProductos(){
     for(const productos of productosVentas){
@@ -29,13 +28,13 @@ function listaProductos(){
 listaProductos()
 
 
-productoA.iva() //que hace esto???
-productoB.iva() //que hace esto???
-productoC.iva() //que hace esto???
 
 
 let cantidadDeCompras = parseInt(prompt("¿Que cantidad de productos desea llevar el dia de hoy? \n-1  \n-2  \n-3" ))
 //que pasa si ingreso cualquier cosa que no sea 1 2 o 3??? armar la condicion correspondiente para la pre-entrega
+
+
+
 let precioFinal = 0;
 
 function calculoPrecio(cantidad, precio){ //que funcion cumple esta funcion??? 
@@ -59,43 +58,36 @@ function calculoStock(cantidad, productos){
 //incluir la iteración FOR adentro de un condicional (si es un numero que haga tal cosa y sino: que haga tal otra ==> volver a preguntar)
 for(i = 0; i < cantidadDeCompras ; i++){
 
+    if(cantidadDeCompras > 3){
+        alert("Seleccionaste un numero que no es correcto");
+        break;
+    }
+
     let CompraInicio = prompt("¿Cual de nuestros productos disponibles quiere comprar? Porfavor elija entre estas opciones: \n" + nuevosProductos.join("\n") )
-    //que sucede si el usuario ingresa cualquier producto que no existe???
+    
+
     let cantidadBuy = parseInt(prompt("Excelente, ¿Cuantos quiere comprar? "))
     //si no existe: por que me pregunta cuantos quiero??? corregir esta logica
 
-    if(CompraInicio == productoA.nombre){ //usar los metodos de comparacion adecuados (toLowerCase(), tuUpperCase(), etc)
-        //todo esto no está mal peeero
-        //todas las condiciones son caaasi iguales:
-            //if (algo==objeto.propiedad) {
-                //calculoStock(cant,prod)
-                    //que aprendimos a usar para no repetir codigo???
-                        //hacer este cambio para la pre-entrega
-        calculoStock(cantidadBuy, productoA)
-       
-    }
-    else if(CompraInicio == productoB.nombre){
-        calculoStock(cantidadBuy , productoB)
-       
-    }
-    else if(CompraInicio == productoC.nombre){
-        calculoStock(cantidadBuy, productoC)
-    
-       
-    }
-    else if(CompraInicio == productoD.nombre){
-        calculoStock(cantidadBuy, productoD)
-    
-       
-    }
-    else if(CompraInicio == productoE.nombre){
-        calculoStock(cantidadBuy, productoE)
-    
-       
-    }
-    else{
-        alert("Lo sentimos, no contamos con ese producto")
-        
+
+    switch(CompraInicio){
+        case "Renders" :
+            calculoStock(cantidadBuy, productoA)
+            break;
+        case "Animations" :
+            calculoStock(cantidadBuy , productoB)
+            break;
+        case "Photos" :
+            calculoStock(cantidadBuy, productoC)
+            break;
+        case "Videos" :
+            calculoStock(cantidadBuy, productoD)
+            break;
+        case "Peluches" :
+            calculoStock(cantidadBuy, productoE)
+            break;
+        default:
+            alert("Lo sentimos, no contamos con ese producto")
     }
       
 }
